1da01c99ac965b001d60d82a3978e1aa238d3786
Andrew Wickham <andrew@awickham.com>
2019-07-12 14:15:15 -0400

    Format superseededBy and supersedes into custom objects so we can match to Get-DbaBuildReference. Some formatting changes too due to vscode auto formatting. (#26)

--- tests/general/Help.Tests.ps1
+++ tests/general/Help.Tests.ps1
@@ -10,7 +10,7 @@
 
 	.PARAMETER SkipTest
 		Disables this test.
-	
+
 	.PARAMETER CommandPath
 		List of paths under which the script files are stored.
 		This test assumes that all functions have their own file that is named after themselves.
@@ -32,17 +32,17 @@
 #>
 [CmdletBinding()]
 Param (
-	[switch]
-	$SkipTest,
-	
-	[string[]]
-	$CommandPath = @("$PSScriptRoot\..\..\functions", "$PSScriptRoot\..\..\internal\functions"),
-	
-	[string]
-	$ModuleName = "SPReplicator",
-	
-	[string]
-	$ExceptionsFile = "$PSScriptRoot\Help.Exceptions.ps1"
+    [switch]
+    $SkipTest,
+
+    [string[]]
+    $CommandPath = @("$PSScriptRoot\..\..\public", "$PSScriptRoot\..\..\private"),
+
+    [string]
+    $ModuleName = "kbupdate",
+
+    [string]
+    $ExceptionsFile = "$PSScriptRoot\Help.Exceptions.ps1"
 )
 if ($SkipTest) { return }
 . $ExceptionsFile
@@ -56,16 +56,16 @@ $commands = Get-Command -Module (Get-Module $ModuleName) -CommandType Cmdlet, Fu
 
 foreach ($command in $commands) {
     $commandName = $command.Name
-    
+
     # Skip all functions that are on the exclusions list
     if ($global:FunctionHelpTestExceptions -contains $commandName) { continue }
-    
+
     # The module-qualified command fails on Microsoft.PowerShell.Archive cmdlets
     $Help = Get-Help $commandName -ErrorAction SilentlyContinue
     $testhelperrors = 0
     $testhelpall = 0
     Describe "Test help for $commandName" {
-        
+
         $testhelpall += 1
         if ($Help.Synopsis -like '*`[`<CommonParameters`>`]*') {
             # If help is not found, synopsis in auto-generated help is the syntax diagram
@@ -74,7 +74,7 @@ foreach ($command in $commands) {
             }
             $testhelperrors += 1
         }
-        
+
         $testhelpall += 1
         if ([String]::IsNullOrEmpty($Help.Description.Text)) {
             # Should be a description for every function
@@ -83,7 +83,7 @@ foreach ($command in $commands) {
             }
             $testhelperrors += 1
         }
-        
+
         $testhelpall += 1
         if ([String]::IsNullOrEmpty(($Help.Examples.Example | Select-Object -First 1).Code)) {
             # Should be at least one example
@@ -92,7 +92,7 @@ foreach ($command in $commands) {
             }
             $testhelperrors += 1
         }
-        
+
         $testhelpall += 1
         if ([String]::IsNullOrEmpty(($Help.Examples.Example.Remarks | Select-Object -First 1).Text)) {
             # Should be at least one example description
@@ -101,27 +101,27 @@ foreach ($command in $commands) {
             }
             $testhelperrors += 1
         }
-        
+
         if ($testhelperrors -eq 0) {
             It "Ran silently $testhelpall tests" {
                 $testhelperrors | Should be 0
             }
         }
-        
+
         $testparamsall = 0
         $testparamserrors = 0
         Context "Test parameter help for $commandName" {
-            
+
             $Common = 'Debug', 'ErrorAction', 'ErrorVariable', 'InformationAction', 'InformationVariable', 'OutBuffer', 'OutVariable',
             'PipelineVariable', 'Verbose', 'WarningAction', 'WarningVariable'
-            
+
             $parameters = $command.ParameterSets.Parameters | Sort-Object -Property Name -Unique | Where-Object Name -notin $common
             $parameterNames = $parameters.Name
             $HelpParameterNames = $Help.Parameters.Parameter.Name | Sort-Object -Unique
             foreach ($parameter in $parameters) {
                 $parameterName = $parameter.Name
                 $parameterHelp = $Help.parameters.parameter | Where-Object Name -EQ $parameterName
-                
+
                 $testparamsall += 1
                 if ([String]::IsNullOrEmpty($parameterHelp.Description.Text)) {
                     # Should be a description for every parameter
@@ -130,7 +130,7 @@ foreach ($command in $commands) {
                     }
                     $testparamserrors += 1
                 }
-                
+
                 $testparamsall += 1
                 $codeMandatory = $parameter.IsMandatory.toString()
                 if ($parameterHelp.Required -ne $codeMandatory) {
@@ -140,11 +140,11 @@ foreach ($command in $commands) {
                     }
                     $testparamserrors += 1
                 }
-                
+
                 if ($HelpTestSkipParameterType[$commandName] -contains $parameterName) { continue }
-                
+
                 $codeType = $parameter.ParameterType.Name
-                
+
                 $testparamsall += 1
                 if ($parameter.ParameterType.IsEnum) {
                     # Enumerations often have issues with the typename not being reliably available
@@ -156,8 +156,7 @@ foreach ($command in $commands) {
                         }
                         $testparamserrors += 1
                     }
-                }
-                elseif ($parameter.ParameterType.FullName -in $HelpTestEnumeratedArrays) {
+                } elseif ($parameter.ParameterType.FullName -in $HelpTestEnumeratedArrays) {
                     # Enumerations often have issues with the typename not being reliably available
                     $names = [Enum]::GetNames($parameter.ParameterType.DeclaredMembers[0].ReturnType)
                     if ($parameterHelp.parameterValueGroup.parameterValue -ne $names) {
@@ -167,8 +166,7 @@ foreach ($command in $commands) {
                         }
                         $testparamserrors += 1
                     }
-                }
-                else {
+                } else {
                     # To avoid calling Trim method on a null object.
                     $helpType = if ($parameterHelp.parameterValue) { $parameterHelp.parameterValue.Trim() }
                     if ($helpType -ne $codeType) {
--- tests/general/PSScriptAnalyzer.Tests.ps1
+++ tests/general/PSScriptAnalyzer.Tests.ps1
@@ -1,10 +1,10 @@
 [CmdletBinding()]
 Param (
-	[switch]
-	$SkipTest,
-	
-	[string[]]
-	$CommandPath = @("$PSScriptRoot\..\..\functions", "$PSScriptRoot\..\..\internal\functions")
+    [switch]
+    $SkipTest,
+
+    [string[]]
+    $CommandPath = @("$PSScriptRoot\..\..\public", "$PSScriptRoot\..\..\private")
 )
 
 if ($SkipTest) { return }
@@ -14,31 +14,26 @@ if ($env:BUILD_BUILDURI -like "vstfs*") { Install-Module PSScriptAnalyzer -Force
 $list = New-Object System.Collections.ArrayList
 
 Describe 'Invoking PSScriptAnalyzer against commandbase' {
-	$commandFiles = Get-ChildItem -Path $CommandPath -Recurse -Filter "*.ps1"
-	$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-	
-	foreach ($file in $commandFiles)
-	{
-		Context "Analyzing $($file.BaseName)" {
-			$analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule PSAvoidTrailingWhitespace, PSAvoidGlobalVars, PSShouldProcess
-			
-			forEach ($rule in $scriptAnalyzerRules)
-			{
-				It "Should pass $rule" {
-					If ($analysis.RuleName -contains $rule)
-					{
-						$analysis | Where-Object RuleName -EQ $rule -outvariable failures | ForEach-Object { $list.Add($_) }
-						
-						1 | Should Be 0
-					}
-					else
-					{
-						0 | Should Be 0
-					}
-				}
-			}
-		}
-	}
+    $commandFiles = Get-ChildItem -Path $CommandPath -Recurse -Filter "*.ps1"
+    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+
+    foreach ($file in $commandFiles) {
+        Context "Analyzing $($file.BaseName)" {
+            $analysis = Invoke-ScriptAnalyzer -Path $file.FullName -ExcludeRule PSAvoidTrailingWhitespace, PSAvoidGlobalVars, PSShouldProcess
+
+            forEach ($rule in $scriptAnalyzerRules) {
+                It "Should pass $rule" {
+                    If ($analysis.RuleName -contains $rule) {
+                        $analysis | Where-Object RuleName -EQ $rule -outvariable failures | ForEach-Object { $list.Add($_) }
+
+                        1 | Should Be 0
+                    } else {
+                        0 | Should Be 0
+                    }
+                }
+            }
+        }
+    }
 }
 
 $list | Out-Default
