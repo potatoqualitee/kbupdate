b6f567ac590facca123e2431c10fac020d329251
Chrissy LeMaire <clemaire@gmail.com>
2020-02-04 20:02:44 +0100

    Add docs

--- public/Select-KbLatest.ps1
+++ public/Select-KbLatest.ps1
@@ -2,7 +2,22 @@
 function Select-KbLatest {
     <#
     .SYNOPSIS
-        Gets the latest from a batch
+        Gets the latest patches from a batch of patches
+
+    .DESCRIPTION
+        Gets the latest patches from a batch of patches, based on Supersedes and SupersededBy
+
+        This command exposes the routine that is used to filter using -Latest in Get-KbUpdate
+
+    .NOTES
+        Author: Chrissy LeMaire (@cl), netnerds.net
+        Copyright: (c) licensed under MIT
+        License: MIT https://opensource.org/licenses/MIT
+
+    .EXAMPLE
+        PS C:\> Get-KbUpdate -Pattern 'sql 2017' | Where-Object Classification -eq Updates | Select-KbLatest
+
+        Selects latest from a batch of patches based on Supersedes and SupersededBy
     #>
     [CmdletBinding()]
     param(
@@ -25,6 +40,3 @@ function Select-KbLatest {
         $matches | Sort-Object UpdateId -Unique
     }
 }
-
-
-
