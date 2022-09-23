54d6542a283dcf65e69b4a9b94f33b02cd7b9cee
Chrissy LeMaire <clemaire@gmail.com>
2022-09-23 16:29:08 +0200

    maybe this

--- kbupdate.psm1
+++ kbupdate.psm1
@@ -140,25 +140,27 @@ if ((Test-Path -Path $linklib)) {
 
     Start-Import -Name Link -ScriptBlock {
         foreach ($linkresult in (Invoke-SqliteQuery -DataSource $script:basedb -Query "select DISTINCT UpdateId, Link from Link")) {
-            $script:linkhash[$linkresult.UpdateId] = $linkresult.Link
+            $script:linkhash[$linkresult.UpdateId] += @($linkresult.Link)
         }
     }
 
     Start-Import -Name Supersedes -ScriptBlock {
         foreach ($superresult in (Invoke-SqliteQuery -DataSource $script:basedb -Query "select UpdateId, KB, Description from Supersedes")) {
-            $script:superhash[$superresult.UpdateId] = [pscustomobject]@{
-                KB          = $superresult.KB
-                Description = $superresult.Description
-            }
+            $script:superhash[$superresult.UpdateId] += @([pscustomobject]@{
+                    KB          = $superresult.KB
+                    Description = $superresult.Description
+                }
+            )
         }
     }
 
     Start-Import -Name SupersededBy -ScriptBlock {
         foreach ($superbyresult in (Invoke-SqliteQuery -DataSource $script:basedb -Query "select UpdateId, KB, Description, Description from SupersededBy")) {
-            $script:superbyhash[$superbyresult.UpdateId] = [pscustomobject]@{
-                KB          = $superbyresult.KB
-                Description = $superbyresult.Description
-            }
+            $script:superbyhash[$superbyresult.UpdateId] += @([pscustomobject]@{
+                    KB          = $superbyresult.KB
+                    Description = $superbyresult.Description
+                }
+            )
         }
     }
 
