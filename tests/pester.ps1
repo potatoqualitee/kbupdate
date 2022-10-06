051772ca555670253ff850088a3ae5ea025217da
Chrissy LeMaire <clemaire@gmail.com>
2022-10-06 14:28:52 +0200

    Update pester.ps1

--- tests/pester.ps1
+++ tests/pester.ps1
@@ -15,16 +15,16 @@ if ($env:BUILD_BUILDURI -like "vstfs*") {
     Write-Host "Installing kbupdate-library" -ForegroundColor Cyan
     Install-Module kbupdate-library -Force -SkipPublisherCheck
     Write-Host "Installing xPSDesiredStateConfiguration" -ForegroundColor Cyan
-    Install-Module xPSDesiredStateConfiguration -Force -SkipPublisherCheck -RequiredVersion 9.2.0
+    Install-Module xPSDesiredStateConfiguration -Force -SkipPublisherCheck -RequiredVersion 9.2.0 -AllowPrerelease
     Write-Host "Installing xWindowsUpdate" -ForegroundColor Cyan
-    Install-Module xWindowsUpdate -Force -SkipPublisherCheck -RequiredVersion 3.0.0
+    Install-Module xWindowsUpdate -Force -SkipPublisherCheck -RequiredVersion 3.0.0 -AllowPrerelease
 }
 
 Write-Host "Loading constants"
 . "$PSScriptRoot\constants.ps1"
 
 Write-Host "Installing xPSDesiredStateConfiguration" -ForegroundColor Cyan
-Install-Module xPSDesiredStateConfiguration -Force -SkipPublisherCheck -RequiredVersion 9.2.0
+Install-Module xPSDesiredStateConfiguration -Force -SkipPublisherCheck -RequiredVersion 9.2.0 -AllowPrerelease
 
 Write-Host "All modules"
 Get-Module -ListAvailable -ErrorAction Ignore | Where-Object Path -like "$env:ProgramFiles*" | Select-Object Name
